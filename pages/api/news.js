84c0128c1c9ac209bf4fde446a058eb2cb046ada
Thiago Laruccia <thiagolaruccia@Thiagos-MacBook-Pro-2.local>
2025-03-04 01:20:37 -0300

    Corrige erro de arquivos duplicados na API

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,9 +1,15 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     const API_KEY = "8467f0ab31504047873c98f5dfa7b16e";
     const API_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => res.status(200).json(data))
-        .catch(error => res.status(500).json({ error: "Erro ao buscar notícias." }));
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error("Erro ao buscar notícias");
+        }
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
