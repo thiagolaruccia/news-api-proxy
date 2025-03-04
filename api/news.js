7f9958646f2250ef145c8c72d5047dc9a5b25738
Thiago Laruccia <thiagolaruccia@Thiagos-MacBook-Pro-2.local>
2025-03-03 23:36:07 -0300

    Corrige API news.js

--- api/news.js
+++ api/news.js
@@ -1,4 +1,12 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
+    const API_KEY = "8467f0ab31504047873c98f5dfa7b16e";
+    const API_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(data => res.status(200).json(data))
+        .catch(error => res.status(500).json({ error: "Erro ao buscar notícias." }));
+}export default async function handler(req, res) {
     const API_KEY = "8467f0ab31504047873c98f5dfa7b16e";
     const API_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
